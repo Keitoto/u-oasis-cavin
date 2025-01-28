49c58d5790e214453e58388d97002023686e8365
Keitoto <keitotooffice@gmail.com>
2025-01-28 16:34:31 +0100

    🚚 move location

--- src/app/_components/Logo.tsx
+++ src/app/_components/Logo.tsx
@@ -1,7 +1,10 @@
+import Image from 'next/image'
+
 export function Logo() {
   return (
     <a href="/" className="flex items-center gap-4 z-10">
       <img height="60" width="60" alt="The Wild Oasis logo" />
+      <Image src={} />
       <span className="text-xl font-semibold text-primary-100">
         The Wild Oasis
       </span>
