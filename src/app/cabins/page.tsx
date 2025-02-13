ae403bed448ad1301411db6040ebd84c02cca297
Keitoto <keitotooffice@gmail.com>
2025-02-13 18:59:07 +0100

    ✨ data fetching with error handling

--- src/app/cabins/page.tsx
+++ src/app/cabins/page.tsx
@@ -1,38 +1,8 @@
-import type { ICabin } from '@/types'
-import CabinCard from '@/app/_components/CabinCard'
+import { CabinList } from '@/app/_components/CabinList'
+import { Spinner } from '@/app/_components/Spinner'
+import { Suspense } from 'react'
 
 export default function Page() {
-  // CHANGE
-  const cabins: ICabin[] = [{
-    id: '1',
-    name: 'Mountain Retreat',
-    maxCapacity: 6,
-    regularPrice: 200,
-    discount: 20,
-    image: 'https://via.placeholder.com/150/92c952',
-  }, {
-    id: '2',
-    name: 'Beachside Bungalow',
-    maxCapacity: 4,
-    regularPrice: 180,
-    discount: 15,
-    image: 'https://via.placeholder.com/150/771796',
-  }, {
-    id: '3',
-    name: 'Forest Hideaway',
-    maxCapacity: 8,
-    regularPrice: 250,
-    discount: 10,
-    image: 'https://via.placeholder.com/150/24f355',
-  }, {
-    id: '4',
-    name: 'Lakeview Lodge',
-    maxCapacity: 10,
-    regularPrice: 300,
-    discount: 25,
-    image: 'https://via.placeholder.com/150/d32776',
-  }]
-
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -46,14 +16,9 @@ export default function Page() {
         away from home. The perfect spot for a peaceful, calm vacation. Welcome
         to paradise.
       </p>
-
-      {cabins.length > 0 && (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-          {cabins.map(cabin => (
-            <CabinCard cabin={cabin} key={cabin.id} />
-          ))}
-        </div>
-      )}
+      <Suspense fallback={<Spinner />}>
+        <CabinList />
+      </Suspense>
     </div>
   )
 }
